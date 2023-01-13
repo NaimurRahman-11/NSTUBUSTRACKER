0a8e84bcf99aaf593cc1cadf9bb83cac7852a1c7
Naimur Rahman <nirzon.naim@gmail.com>
2023-01-13 14:51:02 -0500

    Added PHP Mail

---
+++ mail.php
@@ -0,0 +1,38 @@
+<?php
+include('DatabaseConnection.php');
+
+
+
+
+$sql="SELECT * from subscribers";
+
+$res=mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($res)>0){
+
+
+    while($x=mysqli_fetch_assoc($res)){
+        $receiver = $x['email'];
+        $subject = "Email Test via PHP using Localhost";
+        $body = "Hi, there...This is a test email send from Localhost.";
+        $sender = "From:nirzon.naim@gmail.com";
+
+    if(mail($receiver, $subject, $body, $sender)){
+    echo "Email sent successfully to $receiver";
+}else{
+    echo "Sorry, failed while sending mail!";
+}
+    }
+    
+}
+
+else{
+    echo "No data!";
+}
+
+// if(mail($receiver, $subject, $body, $sender)){
+//     echo "Email sent successfully to $receiver";
+// }else{
+//     echo "Sorry, failed while sending mail!";
+// }
+?>
